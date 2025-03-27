dbf87b664f61bc01ac0850fd9b7f4b617559c877
ss1ngh <singhshreyansh2354@gmail.com>
2025-03-27 09:16:50 +0530

    bugs fixed

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Clock, Target, TrendingUp, Video } from 'lucide-react';
 
 export default function Dashboard() {
