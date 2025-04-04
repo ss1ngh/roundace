0436d39793527d8a2c8423c2cd32962e2ac67268
ss1ngh <singhshreyansh2354@gmail.com>
2025-04-05 05:04:32 +0530

    updated hero section

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -3,15 +3,18 @@ import heroImage from "@/assets/sample-image.jpg"
 
 const HeroSection = () => {
   return (
-    <div className="m-auto grid max-w-[90rem] grid-cols-[5fr_4fr] items-center gap-x-8 px-24 py-30">
+    <div className="m-auto grid max-w-[90rem] grid-cols-[5fr_4fr] items-center gap-x-12 px-24 py-6">
         <div className="mb-6">
-            <h1 className="text-slate-200 mt-16 mb-6 text-6xl/normal font-semibold tracking-tighter">AI Powered Mock Interviews. Ace your Interview Rounds</h1>
+            <h1 className="text-slate-200 mt-16 mb-6 text-6xl/normal font-semibold tracking-tighter">AI-Powered Mock Interviews. Ace your Interview Rounds</h1>
             <p className="text-slate-200 mb-10 text-xl/loose font-semibold-thin">
                 Practice with AI, receive instant feedback, <br/>
                 and continuously improve your performance
             </p>
-            <button className="text-black border-slate-500 bg-blue-500 hover:border-blue-400 hover:bg-blue-300 rounded-xl px-4 py-2 flex items-center">
-                <p className="px-2">Get Started</p>
+            <button 
+                className="text-white border border-white/10 bg-white/20 hover:bg-white/30 
+                            backdrop-blur-lg rounded-xl px-4 py-2 flex items-center w-fit gap-x-2
+                            transition-all">
+                <p className="px-2 font-mono">Get Started</p>
                 <ArrowRight/>
             </button>
         </div>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,24 +1,25 @@
-import BackgroundHeroContainer from "@/components/bg-image"
+// src/pages/LandingPage.tsx
 import Header from "@/components/header"
 import HeroSection from "@/components/hero-section"
 import Navbar from "@/components/Navbar"
+import Reviews from "@/components/reviews"
 
 const LandingPage = () => {
   return(
-    <div className="min-h-screen bg-black">
-      <div className="max-w-full mx-auto px-1">
-        <div className="rounded-b-xl border-t-0 overflow-hidden backdrop-blur-2xl bg-white/10 border border-white/40 shadow-[0_8px_32px_rgba(0,0,0,0.5)]">
-          <BackgroundHeroContainer>
-            <div className="">
-              <Header>
+    <div className="min-h-screen relative">
+      {/* gradient background */}
+      <div className="absolute inset-0 -z-10 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]" />
+      {/* hero section */}
+      <div className="relative h-full">
+        <div className="max-w-full mx-auto px-6 py-4">
+            <Header>
                 <Navbar/>
-                <HeroSection/>
-              </Header>
-            </div>
-          </BackgroundHeroContainer>
+              <HeroSection/>
+              <Reviews/>
+            </Header>
+          </div>
         </div>
       </div>
-    </div>
   )
 }
 
